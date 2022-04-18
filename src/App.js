ccf63bc2060ecbec05eae4e005ec48f1386a6fe7
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-04-18 13:51:11 +0200

    navigation bar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./HomePage/Header/Header";
+import Home from "./HomePage/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Home></Home>
     </div>
   );
 }
