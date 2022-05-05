dccdaefd79ec89cc8a62d887ff06ffb1442be1c9
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-05-06 00:22:35 +0200

    Blog Q1 completed

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -2,8 +2,31 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h2>This is from blog</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-5 col-sm-12">
+          <h2>Authentication vs Authorization</h2>
+          <p>
+            By authentication, we can identify who has permission to login and
+            by authorization, we restrict the use of resources for specific
+            persons. So, authorization is more of like some administrators who
+            has some extra control over the applications. On the other hand,
+            authentication is valid for all users which also includes
+            administrators also.
+          </p>
+        </div>
+        <div className="col-lg-5 col-sm-12">
+          <h2>Authentication vs Authorization</h2>
+          <p>
+            By authentication, we can identify who has permission to login and
+            by authorization, we restrict the use of resources for specific
+            persons. So, authorization is more of like some administrators who
+            has some extra control over the applications. On the other hand,
+            authentication is valid for all users which also includes
+            administrators also.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
