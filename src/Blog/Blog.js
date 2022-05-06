d5c531aeb253d9833ac3016a9115b78f9b9d5e9a
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-05-06 21:48:01 +0200

    blog section completed

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -3,9 +3,9 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-lg-5 col-sm-12">
-          <h2>Authentication vs Authorization</h2>
+      <div className="row mx-auto mt-5 text-center">
+        <div className="col-lg-6 col-sm-12">
+          <h2 className="text-primary mb-3">Authentication vs Authorization</h2>
           <p>
             By authentication, we can identify who has permission to login and
             by authorization, we restrict the use of resources for specific
@@ -15,15 +15,13 @@ const Blog = () => {
             administrators also.
           </p>
         </div>
-        <div className="col-lg-5 col-sm-12">
-          <h2>Authentication vs Authorization</h2>
+        <div className="col-lg-6 col-sm-12">
+          <h2 className="text-primary mb-3">Firebase</h2>
           <p>
-            By authentication, we can identify who has permission to login and
-            by authorization, we restrict the use of resources for specific
-            persons. So, authorization is more of like some administrators who
-            has some extra control over the applications. On the other hand,
-            authentication is valid for all users which also includes
-            administrators also.
+            Firebase offers a authentication service which can be used to
+            control who can login and how to login and registration process and
+            so on. Other than authentication, it also provides hosting services
+            which can be used to host websites.
           </p>
         </div>
       </div>
--- src/HomePage/Review/Review.js
+++ src/HomePage/Review/Review.js
@@ -4,7 +4,7 @@ const Review = ({ review }) => {
   const { name, description, rating } = review;
   return (
     <div className="col-lg-4 col-sm-12 px-3 mx-auto">
-      <div className="container-fluid shadow rounded p-3 bg-light text-center mb-3">
+      <div className="container-fluid shadow rounded p-3 bg-light text-center mb-3 border-top">
         <h4>Name : {name}</h4>
         <h5>Comments : {description}</h5>
         <p>Ratings: {rating} / 10</p>
